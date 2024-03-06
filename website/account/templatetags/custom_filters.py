c513d3f019644e1e19ac56316f6a47258c9b97eb
komarite <ulasgulhan9@gmail.com>
2024-03-06 16:32:56 +0300

    All coin information is displayed on the homepage. A template tag was written for 24-hour changes and volume values.

---
+++ website/account/templatetags/custom_filters.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def multiply(value, arg):
+    return float(value) * arg
+
+@register.filter(name='format_value')
+def format_value(value):
+    if float(value) >= 10**9:
+        return f'{float(value) / 10**9:.2f}B'
+    elif float(value) >= 10**6:
+        return f'{float(value) / 10**6:.2f}M'
+    elif float(value) >= 10**3:
+        return f'{float(value) / 10**3:.2f}K'
+    else:
+        return float(value)
--- website/account/views.py
+++ website/account/views.py
@@ -6,12 +6,22 @@ from django.shortcuts import render
 
 def get_coin_data(request):
     user = request.user
-    url = "https://api.bitget.com/api/v2/spot/public/coins"
+    url = "https://api.bitget.com/api/v2/spot/market/tickers"
     response = requests.get(url)
+    # print(response.json())
+    change24h = []
+    for coin in response.json()['data']:
+            try:
+                percentage_change=((float(coin['high24h']) - float(coin['low24h'])) / float(coin['low24h'])) * 100
+                change24h.append(percentage_change)
+            except ZeroDivisionError:
+                change24h.append(0)
+    
 
     context = {
         'coins': response.json(),
-        'user': user
+        'user': user,
+        'percentage': change24h,
     }
     
     return render(request, 'home.html', context)
