e851256631073bc422a58fc2f48dbb4966b0af0a
komarite <ulasgulhan9@gmail.com>
2024-03-11 16:58:39 +0300

    API models created

--- website/account/models.py
+++ website/account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class BaseAPI(models.Model):
+    api_key     = models.CharField(max_length=200)
+    secret_key  = models.CharField(max_length=200)
+
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self.user_id
+
+
+class BitGetAPI(BaseAPI):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bitget_api')
+    access_passphrase = models.CharField(max_length=200)
+
+
+class BybitAPI(BaseAPI):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bybit_api')
+
+
