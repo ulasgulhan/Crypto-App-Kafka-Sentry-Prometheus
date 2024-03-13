c0260a7216fe7746bad7df6eb4b3c6498f8044ef
komarite <ulasgulhan9@gmail.com>
2024-03-13 23:59:01 +0300

    The APIEndpoints model was created for API endpoints and added to the CryptoMarketPlace class. The Bitget class was reorganized accordingly.

--- website/account/models.py
+++ website/account/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class APIEndpoints(models.Model):
+    api_site_name   = models.CharField(max_length=200)
+    endpoint_name   = models.CharField(max_length=200)
+    endpoint_url    = models.CharField(max_length=200)
+    auth_required   = models.BooleanField()
+    method          = models.CharField(max_length=200)
+
+
 class BaseAPI(models.Model):
     api_key     = models.CharField(max_length=200)
     secret_key  = models.CharField(max_length=200)
--- website/account/services/__init__.py
+++ website/account/services/__init__.py
@@ -1,6 +1,6 @@
 import requests
 import time
-from ..models import BitGetAPI, BybitAPI
+from ..models import BitGetAPI, BybitAPI, APIEndpoints
 from ..utilities import generate_signature
 
 
@@ -13,9 +13,14 @@ class CryptoMarketPlace():
         self.timestamp = None
         self.db_model = None
         self.domain = None
+        self.api_model = APIEndpoints
 
     def generate_headers(self, url=None):
         return None
+    
+
+    def get_api_endpoints(self, website):
+        return APIEndpoints.objects.filter(api_site_name=website)
 
 
     def fetcher(self, auth_header_required=False, url=None, method="GET"):
@@ -38,34 +43,17 @@ class Bitget(CryptoMarketPlace):
         self.domain = "https://api.bitget.com"
 
 
-    def get_api_endpoints(self):
-        return {
-            'account': {'url': '/api/v2/mix/account/account?symbol=btcusdt&productType=USDT-FUTURES&marginCoin=usdt', "auth_required": True, "method": "GET"},
-            'personal_info': {'url': '/api/v2/spot/account/info', "auth_required": True, "method": "GET" },
-            'account_assets':{'url': '/api/v2/spot/account/assets?assetType=all', "auth_required": True, "method": "GET" }, 
-            'coins': {"url":"/api/v2/spot/market/tickers", "auth_required": False, "method": "GET" } 
-        }
-
     def generate_headers(self, endpoint_path):
-        api_info = BitGetAPI.objects.filter(user=self.user)
+        api_info = BitGetAPI.objects.get(user=self.user)
 
         message = self.timestamp + 'GET' + endpoint_path
 
-
-        for api in api_info:
-            access_key = api.api_key
-            access_passphrase = api.access_passphrase
-            secret_key = api.secret_key
-
         headers = {
             'ACCESS-TIMESTAMP': self.timestamp,
-            'ACCESS-KEY': access_key,
-            'ACCESS-PASSPHRASE': access_passphrase,
+            'ACCESS-KEY': api_info.api_key,
+            'ACCESS-PASSPHRASE': api_info.access_passphrase,
+            'ACCESS-SIGN': generate_signature(api_info.secret_key, message)
         }
-
-        signature = generate_signature(secret_key, message)
-
-        headers['ACCESS-SIGN'] = signature
 
         return headers
     
@@ -73,9 +61,9 @@ class Bitget(CryptoMarketPlace):
     def get_api_data(self):
         context = {}
 
-        api_endpoints = self.get_api_endpoints()
+        api_endpoints = self.get_api_endpoints('bitget')
         
-        for endpoint_name, endpoint_data in api_endpoints.items():
-            context[endpoint_name] = self.fetcher(endpoint_data.get("auth_required"), url=endpoint_data.get("url"))
+        for endpoint in api_endpoints:
+            context[endpoint.endpoint_name] = self.fetcher(endpoint.auth_required, url=endpoint.endpoint_url)
 
         return context
