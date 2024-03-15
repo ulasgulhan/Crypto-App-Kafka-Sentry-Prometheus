f477fd14c4f628fa71f3c5836888b3a351d46785
komarite <ulasgulhan9@gmail.com>
2024-03-15 14:32:11 +0300

    OKX coin data fetch and write html

--- website/account/views.py
+++ website/account/views.py
@@ -134,7 +134,7 @@ def okx(request):
 
         api_class = OKX(request.user)
         context = api_class.get_api_data()
-        pprint(context)
+        pprint(context['okx_account_assets'])
         return render(request, 'sites/okx.html', context)
     except Exception as e:
         print(e)
